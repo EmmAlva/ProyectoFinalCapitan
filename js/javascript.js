505928ef0d7843aa1c07690c28a8818b05040723
Emma Tapia <clioemma_28@hotmail.com>
2017-03-06 15:42:32 -0500

    Primer commit

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+function nombre(){
+	var nombre = document.getElementById("nombre");
+	var ingreso = prompt("Ingrese su nombre de Usuario");
+	nombre.innerHTML = "Hola "+ ingreso;
+
+}nombre();
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
